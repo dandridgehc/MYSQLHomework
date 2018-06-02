96ae002f5d667a1481d1302b0a888776c69dead8
Haleigh Dandridge <haleighdandridge@calvisitor-10-105-129-34.calvisitor.1918.berkeley.edu>
2018-06-02 10:31:09 -0700

    inquirer start

--- bamazonCustomer/bamazonCustomer.js
+++ bamazonCustomer/bamazonCustomer.js
@@ -3,7 +3,7 @@ var mysql = require("mysql");
 var inquirer = require("inquirer");
 // require("console.table");
 
-//var selection = process.argv[3]
+
 
 //DATABASE CONNECTION & ERROR HANDLING
 var connection = mysql.createConnection({
@@ -18,7 +18,7 @@ var connection = mysql.createConnection({
   database: "bamazon"
 });
 
-
+//test error handle to test connection, else start bamazon
 connection.connect(function(err) {
   if (err) {
     console.error("error connecting: " + err.stack);
@@ -27,27 +27,78 @@ connection.connect(function(err) {
 });
 
 
-
+//function to get full array of objects from products database 
 function getData(){
     connection.query("SELECT * FROM products", function(err, res) {
         if (err) throw err;
         //console.log(res);
         productList(res)
+        promptCustomerChoice();
         connection.end();
         
       });  
 }
 
+//function to get products listed
 function productList(array) {
+
+  var itemId;  
   for(i=0;i<array.length;i++){
     //console.log(array[i])
-    console.log("ID:", array[i].item_id)
+    itemId = array[i].item_id
+    console.log("ID:", itemId)
     console.log("Product:", array[i].product_name)
     console.log("Price:", array[i].price)
     console.log("Only", array[i].stock_quantity , "left in stock!")
   }
 }
 
+var sampleQuestion = "test this"
+function promptCustomerChoice(input) {
+  // prompt to get custmer input and record it
+  
+  
+    inquirer
+      .prompt({
+        name: "action",
+        type: "list",
+        message: "Pick a product by it's ID#",
+        choices: [
+          "1",
+          "2",
+          "3",
+          "4",
+          "5",
+          "6",
+          "7",
+          "8",
+          "9"
+        ]
+      })
+      .then(function(answer) {
+        switch (answer.action) {
+        case "Find songs by artist":
+          artistSearch();
+          break;
+  
+        case "Find all artists who appear more than once":
+          multiSearch();
+          break;
+  
+        case "Find data within a specific range":
+          rangeSearch();
+          break;
+  
+        case "Search for a specific song":
+          songSearch();
+          break;
+        }
+      });
+  
+  
+  
+
+}
 
 
 
